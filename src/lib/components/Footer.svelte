ac53d13550d819cbf57eb70394917f33c2ebbb2f
lauhard.andreas <lauhard.andreas@gmail.com>
2023-08-28 19:31:58 +0200

    basic setup

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+    const copyright = ()=>{
+        const copyright = `© Andreas Lauhard, ${new Date().getFullYear()} `;
+        return year;
+    }
+</script>
+<footer>
+    <slot name="stock-images">
+
+    </slot>
+    <p>{copyright}</p>
+</footer>
+<style>
+
+</style>
